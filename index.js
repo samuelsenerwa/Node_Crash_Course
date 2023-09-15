428d00f1f84f4b6d5336ecccd35a724f2b26d9a8
samuelsenerwa <samuelsenerwa@gmail.com>
2023-09-15 09:38:49 +0300

    Added dynamic file path

--- index.js
+++ index.js
@@ -54,6 +54,31 @@ const server = http.createServer((req, res) => {
       contentType = "image/jpg";
       break;
   }
+
+  //   Read File
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        // page not found
+        fs,
+          fs.readFile(
+            path.join(__dirname, "public", "404.html"),
+            (err, content) => {
+              res.writeHead(200, { "Content-Type": "text/html" });
+              res.end(content, "utf8");
+            }
+          );
+      } else {
+        // some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
